4fdcb3bdc836d99d76292adf2a3219a80951752f
Lukas Lieb <lukas.lieb@axonivy.com>
2024-09-30 12:40:10 +0200

    XIVY-14994 Add protocol type to exports

--- packages/dataclass-editor/src/index.ts
+++ packages/dataclass-editor/src/index.ts
@@ -2,5 +2,6 @@ export * from './components/dataclass/data/dataclass';
 export { default as DataClassEditor } from './DataClassEditor';
 export * from './protocol/client-json-rpc';
 export * from './protocol/ClientContextProvider';
+export * from './protocol/types';
 export * from './query/query-client';
 export * from './query/QueryProvider';
