e0d93441c6e37f0a7b7cb29ab21ff7669a92de3f
Lukas Giger <lukas.giger@axonivy.com>
2024-10-03 08:40:35 +0200

    fix tests

--- integrations/standalone/tests/integration/jump-to-editors.spec.ts
+++ integrations/standalone/tests/integration/jump-to-editors.spec.ts
@@ -3,14 +3,14 @@ import { DataClassEditor } from '../pageobjects/DataClassEditor';
 
 test('not visible in normal data class', async ({ page }) => {
   const editor = await DataClassEditor.openDataClass(page, 'dataclasses/dataclass/Data.d.json');
-  await expect(editor.title).toHaveText('Data Editor');
+  await expect(editor.title).toHaveText('Data Class - Data');
   await expect(editor.toolbar.getByRole('button', { name: 'Open Process' })).toBeHidden();
   await expect(editor.toolbar.getByRole('button', { name: 'Open Form' })).toBeHidden();
 });
 
 test('visible in form data class', async ({ page }) => {
   const editor = await DataClassEditor.openDataClass(page, 'src_hd/dataclass/form/formData.d.json');
-  await expect(editor.title).toHaveText('Data Editor');
+  await expect(editor.title).toHaveText('Data Class - formData');
   await expect(editor.toolbar.getByRole('button', { name: 'Open Process' })).toBeVisible();
   await expect(editor.toolbar.getByRole('button', { name: 'Open Form' })).toBeVisible();
 });
