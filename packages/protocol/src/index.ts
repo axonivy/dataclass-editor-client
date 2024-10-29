b7bb8de6f3a91b1f2682cfdca1fcfa110a1f6826
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-10-29 12:12:26 +0100

    XIVY-15138 export all generated types

--- packages/protocol/src/index.ts
+++ packages/protocol/src/index.ts
@@ -1,17 +1,3 @@
 export * from './dataclass';
 export * from './types';
-export type {
-  JavaType,
-  DataclassType,
-  Field,
-  Modifier,
-  Association,
-  CascadeType,
-  EntityClass,
-  EntityField,
-  DataClassEditorDataContext,
-  DataClassCombineArgs,
-  TypeSearchRequest,
-  ValidationResult,
-  Severity
-} from './editor';
+export * from './editor';
