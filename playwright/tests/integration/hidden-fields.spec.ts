4d1049c5835fa1eb90593daf95ecfbf8e26aafb9
Lukas Lieb <lukas.lieb@axonivy.com>
2024-12-19 16:37:28 +0100

    XIVY-15662 Hide type switch on hd data editor
    - Hide also the persist flag

--- playwright/tests/integration/hidden-fields.spec.ts
+++ playwright/tests/integration/hidden-fields.spec.ts
@@ -1,16 +1,22 @@
 import { expect, test } from '@playwright/test';
 import { DataClassEditor } from '../pageobjects/DataClassEditor';
 
-test('not visible in normal data class', async ({ page }) => {
+test('normal data class', async ({ page }) => {
   const editor = await DataClassEditor.openDataClass(page, 'dataclasses/dataclass/Data.d.json');
   await expect(editor.title).toHaveText('Data Class - Data');
   await expect(editor.toolbar.getByRole('button', { name: 'Open Process' })).toBeHidden();
   await expect(editor.toolbar.getByRole('button', { name: 'Open Form' })).toBeHidden();
 });
 
-test('visible in form data class', async ({ page }) => {
+test('hd data class', async ({ page }) => {
   const editor = await DataClassEditor.openDataClass(page, 'src_hd/dataclass/form/formData.d.json');
   await expect(editor.title).toHaveText('Data Class - formData');
   await expect(editor.toolbar.getByRole('button', { name: 'Open Process' })).toBeVisible();
   await expect(editor.toolbar.getByRole('button', { name: 'Open Form' })).toBeVisible();
+
+  await editor.detailToggle.locator.click();
+  await expect(editor.detail.dataClass.general.classType.collapsible.locator).toBeHidden();
+
+  await editor.table.row(0).locator.click();
+  await expect(editor.detail.field.general.properties.collapsible.locator).toBeHidden();
 });
